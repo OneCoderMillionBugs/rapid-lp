dfc2152ff8215566a789b8f55a1b2226db53ed43
Ilyas <111341551+OneCoderMillionBugs@users.noreply.github.com>
2024-02-23 05:50:34 +0600

    Add files via upload

--- asynclog/main.py
+++ asynclog/main.py
@@ -1,16 +1,25 @@
 from concurrent_logging import ConcurrentLogger
 from concurrent_logging import concurrent_logging, log_queue
 
+from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import wait
+
 
 @concurrent_logging('%(asctime)s: %(message)s', log_queue)
-def shout(clogger, msg):
-    clogger.info(msg + '!')
+def shout(clogger, pid):
+    for i in range(1000):
+        clogger.info(str(pid) + ': ' + str(i))
 
 
 if __name__ == '__main__':
     cl = ConcurrentLogger(False, True)
     cl.start()
 
-    shout('Hello')
+    futures = []
 
+    with ProcessPoolExecutor(max_workers=4) as exec:
+        for i in range(4):
+            futures.append(exec.submit(shout(i)))
+
+    wait(futures)
     cl.stop()
